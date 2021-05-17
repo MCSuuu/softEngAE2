70b9372407042f9fa0d56f52e065cfcc64099e69
MCSuuu <69879375+MCSuuu@users.noreply.github.com>
2021-05-17 11:27:34 +0100

    Update TeachingReq.java

--- TeachingReq.java
+++ TeachingReq.java
@@ -3,14 +3,13 @@ import java.util.ArrayList;
 public class TeachingReq {
     private String reqTrain;
     private int reqStaffNo;
-    private ArrayList<String> rT;
-    public TeachingReq(String t,int n){ //TeachingReq objects contains required training
-        rT= new ArrayList<String>();
-        rT.add(t);                      //and required number of staffs.
-        reqStaffNo=n;                   //when creating TeachingReq objects,create an ArrayList and
-    }                                   //add training into the arrayList
+
+    public TeachingReq(String t,int n){ //TeachingReq objects contains 1 required training
+        reqTrain = t;                   //and required number of staffs.
+        reqStaffNo=n;
+    }
 
     public String toString(){
-         return "Required training: "+rT+",required number of staffs: "+ reqStaffNo;
+         return "Required training: "+reqTrain+",required number of staffs: "+ reqStaffNo;
     }
 }
